585850b5151536f41cf76875b386c915499200fa
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-03-23 12:38:32 +1100

    Use database utils

--- src/commands/Settings/prefix.ts
+++ src/commands/Settings/prefix.ts
@@ -1,24 +1,18 @@
 import { envParseString } from '#lib/env'
 import { RavenCommand } from '#lib/structures/Command'
+import { readSettings, writeSettings } from '#lib/util'
 import { ApplyOptions } from '@sapphire/decorators'
 import { send } from '@sapphire/plugin-editable-commands'
 import type { SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands'
-import type { Message, Snowflake } from 'discord.js'
+import type { Message } from 'discord.js'
 
 @ApplyOptions<SubCommandPluginCommandOptions>({
   description: 'Set guild prefix',
   subCommands: ['set', 'reset', { input: 'show', default: true }]
 })
 export class Prefix extends RavenCommand {
-  private async settings(guildId: Snowflake) {
-    return await this.container.client.prisma.guildSettings.findUnique({
-      where: {
-        guildId
-      }
-    })
-  }
   public async show(message: Message) {
-    const settings = await this.settings(message.guild!.id)
+    const settings = await readSettings(message.guild!.id)
 
     return await send(message, `Current guild prefix is: ${settings?.prefix ?? envParseString('PREFIX')}`)
   }
@@ -29,14 +23,7 @@ export class Prefix extends RavenCommand {
     if (!prefix) return await send(message, 'You must provide a prefix!')
     if (prefix.length > 4) return await send(message, 'The new prefix cannot be more than 4 letters long!')
 
-    await this.container.client.prisma.guildSettings.update({
-      where: {
-        guildId: message.guild!.id
-      },
-      data: {
-        prefix
-      }
-    })
+    await writeSettings(message.guild!.id, { prefix })
 
     return await send(message, `I've set the Guild prefix to \`${prefix}\``)
   }
@@ -44,14 +31,7 @@ export class Prefix extends RavenCommand {
   public async reset(message: Message) {
     const prefix = envParseString('PREFIX')
 
-    await this.container.client.prisma.guildSettings.update({
-      where: {
-        guildId: message.guild!.id
-      },
-      data: {
-        prefix
-      }
-    })
+    await writeSettings(message.guild!.id, { prefix })
 
     return await send(message, `I've reset the guild prefix back to ${prefix}`)
   }
