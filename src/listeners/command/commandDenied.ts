0516ee78a458df83855c4592b12e241973233d63
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-05-14 10:04:18 +1000

    Update commandError and commandDenied

--- src/listeners/command/commandDenied.ts
+++ src/listeners/command/commandDenied.ts
@@ -7,6 +7,6 @@ export class UserEvent extends Listener<typeof Events.ChatInputCommandDenied> {
     // Use cases for this are for example permissions error when running the `eval` command.
     if (Reflect.get(Object(context), 'silent')) return
 
-    return await interaction.reply({ content, allowedMentions: { users: [interaction.user.id], roles: [] } })
+    return await interaction.reply({ content, allowedMentions: { users: [interaction.user.id], roles: [] }, ephemeral: true })
   }
 }
--- src/listeners/command/commandError.ts
+++ src/listeners/command/commandError.ts
@@ -1,10 +1,13 @@
-import type { ChatInputCommandErrorPayload, Events } from '@sapphire/framework'
-import { Listener } from '@sapphire/framework'
+import { ChatInputCommandErrorPayload, Events, Listener, UserError } from '@sapphire/framework'
 
 export class UserEvent extends Listener<typeof Events.ChatInputCommandError>  {
   public async run(error: Error, { interaction }: ChatInputCommandErrorPayload): Promise<void> {
     // `context: { silent: true }` should make UserError silent:
     // Use cases for this are for example permissions error when running the `eval` command.
-    return await interaction.reply({ content: error.message ?? 'Something went wrong...', allowedMentions: { users: [interaction.user.id], roles: [] }, ephemeral: true })
+    if (error instanceof UserError) {
+      return await interaction.reply({ content: error.message ?? 'Something went wrong...', allowedMentions: { users: [interaction.user.id], roles: [] }, ephemeral: true })
+    }
+
+    return this.container.logger.error(error.message)
   }
 }
